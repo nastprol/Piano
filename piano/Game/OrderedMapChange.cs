b5b9ab0714892ccf24a76aa7785a45fbc49b8bb1
Sapog123 <verasapozhnickova@yandex.ru>
2019-05-16 01:14:38 +0500

    2

---
+++ piano/Game/OrderedMapChange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piano
+{
+    class OrderedKeyCombination : IKeyCombinations
+    {
+        private int index = 0;
+        private int[] combination =
+            new int[] { 0, 1, 1, 2, 3, 3, 4, 5, 6, 7, 8, 8, 5, 4, 6, 6, 7, 2, 3, 5, 3, 5, 3, 2, 2, 1, 0, 7 };
+
+        public PianoKey[] GetNextKeyLine(int numberInWidth, Note note)
+        {
+            var keyLine = new PianoKey[numberInWidth];
+            for (var i = 0; i > numberInWidth; i++)
+                keyLine[i] = new PianoKey();
+            var keyWithNote = combination[index] % numberInWidth;
+            keyLine[keyWithNote] = new PianoKey(note);
+            index = index + 1 < combination.Length ? index + 1 : 0;
+            return keyLine;
+        }
+    }
+}
---
+++ piano/Game/RandKeyMapChange.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Piano
+{
+    class RandKeyMapChange : IMapChange
+    {
+        private Random rnd = new Random();
+
+        public PianoKey[] GetNextKeyLine(int numberInWidth, Note note)
+        {
+            var keyWithNote = rnd.Next(numberInWidth);
+            var keyLine = new PianoKey[numberInWidth];
+            for (var i = 0; i > numberInWidth; i++)
+                keyLine[i] = new PianoKey();
+            keyLine[keyWithNote] = new PianoKey(note);
+            return keyLine;
+        }
+    }
+}

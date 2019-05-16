92da3aa6749c99c64dca118942536e8e7000be5a
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2019-05-17 01:42:29 +0500

    gui works

--- piano/GUI/GameForm.cs
+++ piano/GUI/GameForm.cs
@@ -1,25 +1,27 @@
 ﻿using System;
 using System.Drawing;
-using System.Security.Cryptography;
 using System.Windows.Forms;
-using Piano;
 using Piano.Game.State;
 
 namespace Piano
 {
     public class GameForm : Form
     {
-        private const int ElementSize = 50;
+        private const int ElementSizeHeight = 100;
+        private const int ElementSizeWidth = 50;
         private readonly IGame state;
 
         public GameForm(IGame state)
         {
+
             this.state = state;
             var map = state.GetMap();
-            ClientSize = new Size(map.NumberInWidth * ElementSize, map.NumberInHigh * ElementSize);
+            ClientSize = new Size(map.NumberInWidth * ElementSizeWidth, map.NumberInHigh * ElementSizeHeight);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             var timer = new Timer();
             timer.Tick += TimerTick;
+            timer.Interval = 60;
+            timer.Start();
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -27,8 +29,8 @@ namespace Piano
             var map = state.GetMap();
             for (var i = 0; i < map.NumberInHigh; i++)
             for (var j = 0; j < map.NumberInWidth; j++)
-                e.Graphics.FillRectangle(new SolidBrush(map[i, j].Color), i * ElementSize, j * ElementSize,
-                    ElementSize, ElementSize);
+                e.Graphics.FillRectangle(new SolidBrush(map[map.NumberInHigh - i - 1, j].Color), i * ElementSizeWidth, j * ElementSizeHeight,
+                    ElementSizeWidth, ElementSizeHeight);
         }
 
         private void TimerTick(object sender, EventArgs e)
